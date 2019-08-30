8d1bcb4e5431c98f4b81ae7770405c856a266a9e
Fabio Rodrigues <fabiordrig@gmail.com>
2019-08-30 11:55:13 -0300

    add dependencias

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const router = require('./routes/index');
 
-var app = express();
+
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('helmet')());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token, x-app-name')
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, POST, PUT, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true)
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+})
 
 app.use('/api', router);
 
@@ -29,7 +43,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
